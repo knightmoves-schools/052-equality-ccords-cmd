a50f72bc7506a23ce3ef843ad7cbb3977d110642
ccords-cmd <ccords@anamosa.k12.ia.us>
2025-11-18 16:46:13 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+function priceCheck(price == 1.5) {
+  return match
+};
 
+should create a function named priceCheck that takes a price and returns the string match if the price is equivalent to 1.50
+should return match when the string 1.50 is passed in as an argument to priceCheck
+should not return match when 1.00 is passed in as an argument to priceCheck
+should create a function named strictPriceCheck that takes a price and returns the string match only if the price is a number that is strictly equivalent to 1.50
+should not return match when the string 1.50 is passed in as an argument to strictPriceCheck
+should not return match when 1.00 is passed in as an argument to strictPriceCheck
